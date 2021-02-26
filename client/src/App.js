7d6ea404974150a7fe55481fa3033c3804ca13d2
Anindita Pharikal <anindita.pharikal7@gmail.com>
2021-02-26 16:56:57 -0500

    validations are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,130 +1,164 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Button } from "./components/Button";
-import {Input} from './components/Input';
+import { Input } from './components/Input';
 import Data from "./components/Data";
 import * as math from "mathjs";
 import { Container } from 'reactstrap';
+import { isInteger } from "mathjs";
+
 
 const io = require("socket.io-client");
 const socket = io();
-class App extends Component{
-  constructor(props){
+class App extends Component {
+
+  state = {
+    data: [],
+    input: ""
+  }
+
+  constructor(props) {
     super(props);
-    this.state={
-      data: [],
-      input:""
-    }
-    socket.on(
-			"render",
-			(msg) => {
-        console.log("Listening to client side---------------------");
-				this.getAllData();
-			}
-		);
   }
+
   //to fetch all data while loading the
-componentDidMount() {
-		this.getAllData();
-}
-
-addToInput=val=>{
-  this.setState({input: this.state.input+val});
-}
-
-backspace = () => {
-		this.setState(() => ({
-			input: this.state.input.slice(0, -1),
-		}));
-};
-reset=()=>{
-  this.setState({input:""});
-}
-
-calculate = () => {
-		let value = "";
-		try {
-			value = math.evaluate(this.state.input);
-		} catch (err) {
-			value = "Invalid Input";
-		}
-		if (value !== "Invalid Input") {
-			this.postRecord(value);
-			this.send();
-		}
-    this.setState({ input: value });
-	};
-getAllData = () => {
-  console.log("getAllData called");
-		fetch("/api/data")
-			.then((res) => res.json())
-			.then((data) => this.setState({data: data }));
-};
-postRecord = (resp) => {
-		const reverseTime = 1000000000000 - new Date().getTime();
-		fetch("api/data", {
-			method: "POST",
-			body: JSON.stringify({
-				time: reverseTime,
-				value: `${this.state.input}=${resp}`,
-			}),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((res) => res.json)
-			.then(this.getAllData());
-};
-send = () => {
-		socket.emit("new data published");
-};
-render(){
-  const overrideStyle={
-    "marginRight": "0px",
-    "marginLeft": "0px"
+  componentDidMount() {
+    socket.on(
+      "render",
+      (msg) => {
+        this.getAllData();
+      }
+    );
+    document.addEventListener("keydown", this._handleKeyDown);
+    this.getAllData();
   }
-    return (
-    <Container className="themed-container">
-    <div className="app">
-      <div className="calc-wrapper">
-        <Input input={this.state.input}></Input>
-        <div className="row" style={overrideStyle}>
-          <Button handleClick={this.addToInput}>7</Button>
-          <Button handleClick={this.addToInput}>8</Button>
-          <Button handleClick={this.addToInput}>9</Button>
-          <Button handleClick={this.addToInput}>/</Button>
-        </div>
-        <div className="row" style={overrideStyle}>
-          <Button handleClick={this.addToInput}>4</Button>
-          <Button handleClick={this.addToInput}>5</Button>
-          <Button handleClick={this.addToInput}>6</Button>
-          <Button handleClick={this.addToInput}>*</Button>
-        </div>
-        <div className="row" style={overrideStyle}>
-          <Button handleClick={this.addToInput}>1</Button>
-          <Button handleClick={this.addToInput}>2</Button>
-          <Button handleClick={this.addToInput}>3</Button>
-          <Button handleClick={this.addToInput}>+</Button>
-        </div>
-        <div className="row" style={overrideStyle}>
-          <Button handleClick={this.addToInput}>.</Button>
-          <Button handleClick={this.addToInput}>0</Button>
-          <Button handleClick={()=>this.calculate()}>=</Button>
-          <Button handleClick={this.addToInput}>-</Button>
-        </div>
-        <div className="row" style={overrideStyle}>
-          <Button handleClick={()=>this.backspace()}>CE</Button>
-          <Button handleClick={()=>this.reset()}>AC</Button>
-        </div>
-      </div>
-      <div className="data-wrapper">
-					<Data data={this.state.data} />
-			</div>
-    </div>
-    </Container>
-  );
 
-  } 
+  addToInput = val => {
+    console.log(this.state.input);
+    let lastChar = this.state.input.toString().charAt(this.state.input.length - 1);
+    console.log(lastChar);
+    if ((lastChar === "/" || lastChar === "*" || lastChar === "+" || lastChar === "-") && (val === "/" || val === "*"))
+      return;
+    this.setState({ input: this.state.input + val });
+  }
+  _handleKeyDown = (event) => {
+    if (event.key === "+" || event.key === "-" || event.key === "*" || event.keyCode === 8 || event.key === "/"
+      || event.key === "." || event.keyCode === 13 || isInteger(parseInt(event.key))) {
+      switch (event.keyCode) {
+        case 13:
+          this.calculate();
+          break;
+        case 8:
+          this.backspace();
+          break;
+        default:
+          this.addToInput(event.key);
+          break;
+      }
+    }
+  }
+
+  backspace = () => {
+    let val = this.state.input.toString();
+    this.setState(() => ({
+      input: val.slice(0, -1),
+    }));
+  };
+  reset = () => {
+    this.setState({ input: "" });
+  }
+
+  calculate = () => {
+    let value = "";
+    let lastChar = this.state.input.toString().charAt(this.state.input.length - 1);
+    if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/" || lastChar === ".") return;
+    try {
+      console.log(this.state.input);
+      if (!this.state.input) return;
+      value = math.evaluate(this.state.input);
+    } catch (err) {
+      value = "Invalid Input";
+    }
+    if (value !== "Invalid Input") {
+      this.postRecord(value);
+      this.send();
+    }
+    this.setState({ input: value });
+  };
+  getAllData = () => {
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((data) => this.setState({ data: data }));
+  };
+  postRecord = (resp) => {
+    const reverseTime = 1000000000000 - new Date().getTime();
+    fetch("api/data", {
+      method: "POST",
+      body: JSON.stringify({
+        time: reverseTime,
+        value: `${this.state.input}=${resp}`,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json)
+      .then(this.getAllData());
+  };
+  send = () => {
+    socket.emit("new data published");
+  };
+  render() {
+    const overrideStyle = {
+      "marginRight": "0px",
+      "marginLeft": "0px"
+    }
+    return (
+      <Container className="themed-container">
+        <div className="app">
+          <div className="calc-wrapper">
+            <Input input={this.state.input}></Input>
+            <div className="row" style={overrideStyle}>
+              <Button handleClick={this.addToInput}>7</Button>
+              <Button handleClick={this.addToInput}>8</Button>
+              <Button handleClick={this.addToInput}>9</Button>
+              <Button handleClick={this.addToInput}>/</Button>
+            </div>
+            <div className="row" style={overrideStyle}>
+              <Button handleClick={this.addToInput}>4</Button>
+              <Button handleClick={this.addToInput}>5</Button>
+              <Button handleClick={this.addToInput}>6</Button>
+              <Button handleClick={this.addToInput}>*</Button>
+            </div>
+            <div className="row" style={overrideStyle}>
+              <Button handleClick={this.addToInput}>1</Button>
+              <Button handleClick={this.addToInput}>2</Button>
+              <Button handleClick={this.addToInput}>3</Button>
+              <Button handleClick={this.addToInput}>+</Button>
+            </div>
+            <div className="row" style={overrideStyle}>
+              <Button handleClick={this.addToInput}>.</Button>
+              <Button handleClick={this.addToInput}>0</Button>
+              <Button handleClick={() => this.calculate()}>=</Button>
+              <Button handleClick={this.addToInput}>-</Button>
+            </div>
+            <div className="row" style={overrideStyle}>
+              <Button handleClick={() => this.backspace()}>CE</Button>
+              <Button handleClick={() => this.reset()}>AC</Button>
+            </div>
+          </div>
+          <div className="data-wrapper">
+            <Data data={this.state.data} />
+          </div>
+        </div>
+      </Container>
+    );
+
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this._handleKeyDown);
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const firebase=require("firebase");
+const firebase = require("firebase");
 const cors = require('cors');
 const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const config=firebase.initializeApp({
+const config = firebase.initializeApp({
   apiKey: "AIzaSyCYiO3qgwY0SRdmt6yBh2LN2O9mfun0SQI",
   authDomain: "calculator-web-app-6202a.firebaseapp.com",
   databaseURL: "https://calculator-web-app-6202a-default-rtdb.firebaseio.com",
@@ -18,20 +18,12 @@ const port = process.env.PORT || 4000;
 const server = app.listen(port);
 require("dotenv").config();
 
-
 app.use(cors());
-// app.options('*', cors());
-
-//To test 
-app.post('/', (req,res) => {
-    const value = req.body.value
-    db.ref().push(value)
-    res.status(200).send("Suceess..")
-})
+app.use(bodyParser.json())
 
 //app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 //WebSocket
- const io = require('socket.io')(server, {
+const io = require('socket.io')(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
@@ -39,48 +31,43 @@ app.post('/', (req,res) => {
   }
 });
 io.on('connection', socket => {
-  socket.on('new data published', function(msg){
-    console.log("listening to server side---------------------");
+  socket.on('new data published', function (msg) {
     io.emit('render', msg);
   });
 })
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(bodyParser.json())
 
 //To fetch data from firebase
 app.get('/api/data', (req, res) => {
   var arrayResult = []
   const query = ref.orderByChild("timeStamp").limitToFirst(10)
   query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      arrayResult.push(childSnapshot.val().value);
-  });
-  res.json(arrayResult)
-});
+    .then(function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        arrayResult.push(childSnapshot.val().value);
+      });
+      res.json(arrayResult)
+    });
 })
 
 //To save data in Firebase
-app.post(`/api/data`, (req, res) =>{
+app.post(`/api/data`, (req, res) => {
   console.log(req.body);
   const recordTime = req.body['time']
   const recordVal = req.body['value']
-  
+
   db.ref('/data').push({
     "timeStamp": recordTime,
     "value": recordVal
   })
   res.status(200)
   res.send("Success!")
-} )
+})
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
