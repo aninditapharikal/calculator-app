ae0a2c47dd0778950899add45a492187a4363f03
Anindita Pharikal <anindita.pharikal7@gmail.com>
2021-02-26 01:59:51 -0500

    socket origin modified 1

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors(corsOptions))
 //WebSocket
 const io = require('socket.io')(server,{
     cors: {
-                origin: ["http://localhost:3000/"],
+                origin: ["http://localhost:4000/socket.io/"],
                 methods: ["GET", "POST"],
                 credentials: true,
                 transports: ['websocket', 'polling'],
